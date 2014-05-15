d321ab95bef5653647e92c49026592c3b6ea8613
gotqn <george_27@abv.bg>
2014-05-16 00:21:02 +0300

    Initial commit

--- lib/assets/thumbnail_hover_effect.rb
+++ lib/assets/thumbnail_hover_effect.rb
@@ -1,6 +1,6 @@
-require './thumbnail_hover_effect/version'
-require './thumbnail_hover_effect/engine'
-require './thumbnail_hover_effect/image'
+require 'thumbnail_hover_effect/version'
+require 'thumbnail_hover_effect/engine'
+require 'thumbnail_hover_effect/image'
 
 module ThumbnailHoverEffect
   # Your code goes here...
